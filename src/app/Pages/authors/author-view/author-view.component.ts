5641d380f2437078259801357652f29f0894e683
Momen Mamdouh <momenhelmi8@gmail.com>
2024-08-12 18:11:11 +0300

    added routing to author-view and waiting for db to continue

--- src/app/Pages/authors/author-view/author-view.component.ts
+++ src/app/Pages/authors/author-view/author-view.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AuthorsService } from '../../../core/services/authors/authors.service';
 
 @Component({
   selector: 'app-author-view',
   standalone: true,
   imports: [],
   templateUrl: './author-view.component.html',
-  styleUrl: './author-view.component.css'
+  styleUrl: './author-view.component.css',
 })
 export class AuthorViewComponent {
+  @Input() id: any;
+  author!: any;
+  authors!: any;
 
+  constructor(private authorsService: AuthorsService) {}
+
+  ngOnInit() {
+    this.authorsService.getAuthors().subscribe((res) => {
+      this.authors = res;
+      this.author = this.authors.find((author: any) => author.authorId == this.id);
+    });
+  }
+
+  getStarArray(rating: number): number[] {
+    return Array(Math.round(rating)).fill(0);
+  }
+
+  onAuthorClick(author: string) {
+    console.log(author);
+  }
 }
