be93abd3a2d95180457ca4f96c37792f125b6f15
Momen Mamdouh <momenhelmi8@gmail.com>
2024-08-12 16:43:21 +0300

    added:author service, finished: authors-list

--- src/app/Pages/authors/authors-list/authors-list.component.ts
+++ src/app/Pages/authors/authors-list/authors-list.component.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { AuthorsService } from '../../../core/services/authors/authors.service';
 
 @Component({
   selector: 'app-authors-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgxPaginationModule],
   templateUrl: './authors-list.component.html',
   styleUrl: './authors-list.component.css'
 })
 export class AuthorsListComponent {
+  p:number = 1;
+  authors!:any;
+  constructor(
+    private router:Router,
+    private authorsService: AuthorsService
+  ){}
 
+  ngOnInit(){
+    this.authorsService.getAuthors().subscribe((res)=> {
+      this.authors = res;
+    })
+  }
+
+  onProductClick(id:number) {
+    this.router.navigate(['/authors', id]);
+  }
 }
