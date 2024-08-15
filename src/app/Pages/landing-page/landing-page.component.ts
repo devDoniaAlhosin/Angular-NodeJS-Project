c10fc97598c9908988e21f7eb4c46d255adcdff5
donia <donia@gmail.com>
2024-08-15 21:06:48 +0300

    Last Updates

--- src/app/Pages/landing-page/landing-page.component.ts
+++ src/app/Pages/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { faAmazon } from '@fortawesome/free-brands-svg-icons/faAmazon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faApple } from '@fortawesome/free-brands-svg-icons/faApple';
 import { RouterLink } from '@angular/router';
 import { faPlay } from '@fortawesome/free-solid-svg-icons/faPlay';
+
 @Component({
   selector: 'app-landing-page',
   standalone: true,
@@ -14,5 +15,5 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons/faPlay';
 export class LandingPageComponent {
   faAmazon = faAmazon;
   faApple = faApple;
-  faPlay = faPlay
+  faPlay = faPlay;
 }
